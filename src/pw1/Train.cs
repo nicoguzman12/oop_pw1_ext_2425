4fe84def48a57d737bcccd354b57b978cd068c34
nicoguzman12 <9403538@alumnos.ufv.es>
2025-06-11 11:10:51 +0200

    Adding atributes to Train Class

--- src/pw1/Train.cs
+++ src/pw1/Train.cs
@@ -2,5 +2,13 @@ using System;
 
 namespace TrainSimulationApp
 {
+    abstract class Train
+    {
+        string ID { get; set; }
+        int ArrivalTime { get; set; }
+        string Type { get; set; }
+        public TrainStatus Status { get; set; }
+    }
 
+    
 }

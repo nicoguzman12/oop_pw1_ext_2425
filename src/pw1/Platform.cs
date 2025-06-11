30682a129ed269a6206a3b0c78c3f82cc6d6c7c4
nicoguzman12 <9403538@alumnos.ufv.es>
2025-06-11 12:40:49 +0200

    Created platform class and its atributes

--- src/pw1/Platform.cs
+++ src/pw1/Platform.cs
@@ -2,5 +2,12 @@ using System;
 
 namespace TrainSimulationApp
 {
+    public class Platform()
+    {
+        public int PlatformNumber { get; set; }
+        public bool IsOccupied { get; set; }
+        public Train? CurrentTrain { get; set; }
+    }
+
 
 }

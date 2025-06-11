3bf2447bfd959966728604b88f5c19ee13f2c5af
nicoguzman12 <9403538@alumnos.ufv.es>
2025-06-11 12:14:50 +0200

    Creating FreightTrain constructor

--- src/pw1/PassengerTrain.cs
+++ src/pw1/PassengerTrain.cs
@@ -7,5 +7,10 @@ namespace TrainSimulationApp
         public int MaxWeight { get; set; }
         public string? FreightType { get; set; }
     }
-
+    public FreightTrain(string ID, int ArrivalTime, string Type, int maxWeight, string freightType)
+            : base(ID, ArrivalTime, Type)
+        {
+            MaxWeight = maxWeight;
+            FreightType = freightType;
+        }
 }

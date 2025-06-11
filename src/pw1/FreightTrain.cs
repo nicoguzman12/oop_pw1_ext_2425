40e960f2b165dc2679ad8bbea2448fdff0e13bd4
nicoguzman12 <9403538@alumnos.ufv.es>
2025-06-11 12:11:07 +0200

    Created the method DisplayInfo

--- src/pw1/FreightTrain.cs
+++ src/pw1/FreightTrain.cs
@@ -11,7 +11,16 @@ namespace TrainSimulationApp
         public PassengerTrain(string ID, int ArrivalTime, string Type, int NumberofCarriages, int Capacity) : base(ID, ArrivalTime, Type) //base allows reuse and extend the class functionality
         {
             this.NumberofCarriages = NumberofCarriages;
-            this.Capacity = Capacity; 
+            this.Capacity = Capacity;
+        }
+
+        public override void DisplayInfo() //it does not take arguments because it directly uses the internal properties
+        {
+            base.DisplayInfo();
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine($"  -Number of Carriages: {NumberofCarriages}");
+            Console.WriteLine($"  -Capacity: {Capacity} passengers");
+            Console.WriteLine("---------------------------------");
         }
     }
 

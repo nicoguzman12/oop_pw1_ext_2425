f3a306ad115211d3f11abf6fdae5b52231b5c5a6
nicoguzman12 <9403538@alumnos.ufv.es>
2025-06-11 11:16:45 +0200

    Creating the train constructor

--- src/pw1/Train.cs
+++ src/pw1/Train.cs
@@ -8,7 +8,14 @@ namespace TrainSimulationApp
         int ArrivalTime { get; set; }
         string Type { get; set; }
         public TrainStatus Status { get; set; }
-    }
+        public Train(string ID, int ArrivalTime, string Type)
+        {
+            this.ID = ID;
+            this.ArrivalTime = ArrivalTime;
+            this.Type = Type;
+            this.Status = TrainStatus.OnRoute; //by logic I initialize it on the way to the station
 
-    
+
+        }
+    }
 }

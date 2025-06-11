68875a9a888cbbd1be37cea25f940dd6a15f4ca4
nicoguzman12 <9403538@alumnos.ufv.es>
2025-06-11 13:33:46 +0200

    adding the contructor and a loop to create the platforms

--- src/pw1/Station.cs
+++ src/pw1/Station.cs
@@ -7,5 +7,15 @@ namespace TrainSimulationApp
         public string Name { get; set; }
         public List<Platform> Platforms { get; set; }
 
+        public Station(string name, int numberOfPlatforms)
+        {
+            this.Name = name;
+            Platforms = new List<Platform>();
+
+            for (int i = 1; i <= numberOfPlatforms; i++)
+            {
+                Platforms.Add(new Platform(i));
+            }
+        }
     }
 }

3a246e79790b2f6f02808e8775a16b956c527bed
nicoguzman12 <9403538@alumnos.ufv.es>
2025-06-11 12:13:14 +0200

    adding specific atributes of freightTrain and using inheritance

--- src/pw1/PassengerTrain.cs
+++ src/pw1/PassengerTrain.cs
@@ -2,5 +2,10 @@ using System;
 
 namespace TrainSimulationApp
 {
+    class FreightTrain : Train
+    {
+        public int MaxWeight { get; set; }
+        public string? FreightType { get; set; }
+    }
 
 }

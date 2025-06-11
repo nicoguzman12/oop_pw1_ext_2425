d4621ec3fab1dff793ef6fc5b967c1fff1770122
nicoguzman12 <9403538@alumnos.ufv.es>
2025-06-11 17:19:36 +0200

    creating an atribute and the loop to ask user how much platforms want

--- src/pw1/Program.cs
+++ src/pw1/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Write your code here for practical work 1!!!");
+﻿using System;
+
+namespace TrainSimulationApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to UFV train station");
+
+            int numberOfPlatforms;
+
+            while (true) //infinite loop until it find a break
+            {
+                Console.Write("Enter the number of platforms: ");
+                if (int.TryParse(Console.ReadLine(), out numberOfPlatforms) && numberOfPlatforms > 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid number. Please enter a valid positive integer.");
+                }          
+            }
+        }
+    }
+}

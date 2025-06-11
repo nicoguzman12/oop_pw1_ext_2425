e148e9b173284efc71520ac5befcff5f9f4d191b
nicoguzman12 <9403538@alumnos.ufv.es>
2025-06-11 12:17:21 +0200

    creating the method to display info

--- src/pw1/PassengerTrain.cs
+++ src/pw1/PassengerTrain.cs
@@ -7,10 +7,17 @@ namespace TrainSimulationApp
         public int MaxWeight { get; set; }
         public string? FreightType { get; set; }
     }
-    public FreightTrain(string ID, int ArrivalTime, string Type, int maxWeight, string freightType)
-            : base(ID, ArrivalTime, Type)
-        {
-            MaxWeight = maxWeight;
-            FreightType = freightType;
-        }
+    public FreightTrain(string ID, int ArrivalTime, string Type, int maxWeight, string freightType): base(ID, ArrivalTime, Type)
+    {
+        MaxWeight = maxWeight;
+        FreightType = freightType;
+    }
+    public override void DisplayInfo() //it does not take arguments because it directly uses the internal properties
+    {
+        base.DisplayInfo();
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine($"  -Max weight: {MaxWeight} tons");
+        Console.WriteLine($"  -Freight type: {FreightType}");
+        Console.WriteLine("---------------------------------");
+    }
 }

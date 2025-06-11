fe9d38c8194cdb0e66cf1c680733272d97b7efcf
nicoguzman12 <9403538@alumnos.ufv.es>
2025-06-11 13:31:17 +0200

    Creating class and atributes

--- src/pw1/Station.cs
+++ src/pw1/Station.cs
@@ -2,5 +2,10 @@ using System;
 
 namespace TrainSimulationApp
 {
+    public class Station
+    {
+        public string Name { get; set; }
+        public List<Platform> Platforms { get; set; }
 
+    }
 }

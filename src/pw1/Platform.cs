7f2d46aa9c54c755c65437047265db096e8497ba
nicoguzman12 <9403538@alumnos.ufv.es>
2025-06-11 12:41:39 +0200

    created the constructor of the class

--- src/pw1/Platform.cs
+++ src/pw1/Platform.cs
@@ -2,12 +2,17 @@ using System;
 
 namespace TrainSimulationApp
 {
-    public class Platform()
+    public class Platform
     {
         public int PlatformNumber { get; set; }
         public bool IsOccupied { get; set; }
         public Train? CurrentTrain { get; set; }
     }
 
-
+    public Platform(int platformNumber)
+        {
+            this.PlatformNumber = platformNumber;
+            this.IsOccupied = false;
+            this.CurrentTrain = null;            
+    }
 }

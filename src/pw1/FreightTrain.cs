a257fc8922e8b0f40e133a5a905f2573a6add4aa
nicoguzman12 <9403538@alumnos.ufv.es>
2025-06-11 12:04:10 +0200

    Used inheritance (Passenger inherits from Train) and implemented specific atributes

--- src/pw1/FreightTrain.cs
+++ src/pw1/FreightTrain.cs
@@ -3,4 +3,16 @@ using System;
 namespace TrainSimulationApp
 {
 
+    class PassengerTrain : Train
+    {
+        public int NumberofCarriages { get; set; }
+        public int Capacity { get; set; }
+
+        public PassengerTrain(string ID, int ArrivalTime, string Type, int NumberofCarriages, int Capacity) : base(ID, ArrivalTime, Type)
+        {
+            this.NumberofCarriages = NumberofCarriages;
+            this.Capacity = Capacity;
+        }
+    }
+
 }

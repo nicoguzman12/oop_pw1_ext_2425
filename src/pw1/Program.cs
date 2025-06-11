cb1a60461be04a06d7dc317cf5f73ee9bb410edb
nicoguzman12 <9403538@alumnos.ufv.es>
2025-06-11 22:36:49 +0200

    Creating the method that reads the csv file and evaluates what it contains

--- src/pw1/Program.cs
+++ src/pw1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace TrainSimulationApp
 {
@@ -32,10 +33,53 @@ namespace TrainSimulationApp
                 Console.WriteLine("2. Start simulation");
                 Console.WriteLine("3. Display system state");
                 Console.WriteLine("4. Exit");
-                Console.WriteLine("-----------------------\n");              
+                Console.WriteLine("-----------------------\n");
                 Console.Write("Choose an option: ");
-                string? choice = Console.ReadLine(); 
+                string? choice = Console.ReadLine();
             }
+        }
+
+        public static List<Train> ReadTrainsFromScv(string filePath)
+        {
+            var trains = new List<Train>();
+
+            try
+            {
+                var lines = File.ReadAllLines(filePath);
+
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    var parts = lines[i].Split(',');
+
+                    if (parts.Length < 5) //evaluating if the are 5 columns
+                    {
+                        continue;
+                    }
+
+                    string id = parts[0];
+                    int arrivalTime = int.Parse(parts[1]);
+                    string type = parts[2];
+
+                    if (type.ToLower() == "passenger")
+                    {
+                        int numberOfCarriages = int.Parse(parts[3]);
+                        int capacity = int.Parse(parts[4]);
+                        trains.Add(new PassengerTrain(id, arrivalTime, type, numberOfCarriages, capacity));
+                    }
+                    else if (type.ToLower() == "freight")
+                    {
+                        int maxWeight = int.Parse(parts[3]);
+                        string freightType = parts[4];
+                        trains.Add(new FreightTrain(id, arrivalTime, type, maxWeight, freightType));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred reading CSV: " + ex.Message);
+            }
+
+            return trains;
         }
     }
 }
